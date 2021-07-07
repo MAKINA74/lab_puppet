177ef3e8de9e2e06b1f6410ade2a045c31a308db
MAKINA74 <makina.mehmet@outlook.fr>
2021-07-07 14:45:31 +0000

    wiki.pp V2

--- wiki.pp
+++ wiki.pp
@@ -8,14 +8,17 @@ package {
 }
 
 
-exec { 
 
-    'téléchargement de dokuwiki':
-    path    => ['/usr/bin'],
-    command => 'wget -O /usr/src/dokuwiki.tgz \
-  https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz ';
-  
-    
+file { 'télécharger dokuwiki':
+  ensure         => 'present',
+  source         => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+  checksum_value => '8867b6a5d71ecb5203402fe5e8fa18c9',
+  path           => '/usr/src/dokuwiki.tgz',
+
+}
+
+exec { 
+      
     'Décompresser le dossier':
     path    => ['/usr/bin'],
     cwd     => '/usr/src',
